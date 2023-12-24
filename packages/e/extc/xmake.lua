5e03033563bb6f3c7c18c55fbb7ac05130bdb29f
Apsapeh <timofeykirichenko@gmail.com>
2023-12-24 20:34:43 +0300

    commit

--- packages/e/extc/xmake.lua
+++ packages/e/extc/xmake.lua
@@ -5,8 +5,11 @@ package("extc")
     add_version("last", "f9d16e95c3a1de53760c0dedae21221f67cf9bfd")
     --add_versions("1.5", "7084070e2d4c47dfc49ae9260b1e462628ad913f")
 
+    on_load(function (package)
+        package:add("includedirs", "include")
+    end)
+
     on_install(function (package)
-	package:add("includedirs", "include/")
         local configs = {}
         if package:config("shared") then
             configs.kind = "shared"
@@ -16,5 +19,5 @@ package("extc")
 
     on_test(function (package)
         -- TODO check includes and interfaces
-        assert(package:has_cfuncs("init_vec", {includes = "extc_vec.h"})
+        assert(package:has_cfuncs("init_vec", {includes = "extc_vec.h"}))
     end)
