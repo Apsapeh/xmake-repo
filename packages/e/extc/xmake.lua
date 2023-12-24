a0810ef4687c15ac5e282eb0c8bf1e192e44a531
Apsapeh <timofeykirichenko@gmail.com>
2023-12-24 20:22:26 +0300

    commit

--- packages/e/extc/xmake.lua
+++ packages/e/extc/xmake.lua
@@ -2,7 +2,8 @@ package("extc")
     set_description("The extc package")
 
     set_urls("https://github.com/Apsapeh/extc.git")
-    add_versions("1.5", "7084070e2d4c47dfc49ae9260b1e462628ad913f")
+    add_version("last", "f9d16e95c3a1de53760c0dedae21221f67cf9bfd")
+    --add_versions("1.5", "7084070e2d4c47dfc49ae9260b1e462628ad913f")
 
     on_install(function (package)
 	package:add("includedirs", "include/")
@@ -15,5 +16,5 @@ package("extc")
 
     on_test(function (package)
         -- TODO check includes and interfaces
-        -- assert(package:has_cfuncs("foo", {includes = "foo.h"})
+        assert(package:has_cfuncs("init_vec", {includes = "extc_vec.h"})
     end)
