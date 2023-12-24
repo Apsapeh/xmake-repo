a1072592780aff591768b61922e264c645620db8
Apsapeh <timofeykirichenko@gmail.com>
2023-12-24 20:48:28 +0300

    commit

--- packages/e/extc/xmake.lua
+++ packages/e/extc/xmake.lua
@@ -14,7 +14,7 @@ package("extc")
         if package:config("shared") then
             configs.kind = "shared"
         end
-        import("package.tools.xmake").install(package, configs)
+        import("package.tools.xmake").install(package)
     end)
 
     on_test(function (package)
