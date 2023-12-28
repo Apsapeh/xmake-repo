0606f555afb1cc3db4895d22f11c6441f0275830
Apsapeh <timofeykirichenko@gmail.com>
2023-12-28 20:00:45 +0300

    Upd

---
+++ packages/o/old3de/xmake.lua
@@ -0,0 +1,19 @@
+package("old3de")
+    set_description("The old3de package")
+    add_deps("reactphysics3d")
+
+    add_urls("https://github.com/Apsapeh/xmake-repo")
+    add_versions("1.0", "<shasum256 or gitcommit>")
+
+    on_install(function (package)
+        local configs = {}
+        if package:config("shared") then
+            configs.kind = "shared"
+        end
+        import("package.tools.xmake").install(package, configs)
+    end)
+
+    on_test(function (package)
+        -- TODO check includes and interfaces
+        -- assert(package:has_cfuncs("foo", {includes = "foo.h"})
+    end)
---
+++ packages/r/reactphysics3d/xmake.lua
@@ -0,0 +1,18 @@
+package("reactphysics3d")
+    set_description("The reactphysics3d package")
+
+    add_urls("https://github.com/Apsapeh/xmake-repo")
+    add_versions("1.0", "<shasum256 or gitcommit>")
+
+    on_install(function (package)
+        local configs = {}
+        if package:config("shared") then
+            configs.kind = "shared"
+        end
+        import("package.tools.xmake").install(package, configs)
+    end)
+
+    on_test(function (package)
+        -- TODO check includes and interfaces
+        -- assert(package:has_cfuncs("foo", {includes = "foo.h"})
+    end)
