2c73c4019ff7fb6b07f120dcb6cef5c7429ce6d2
Apsapeh <timofeykirichenko@gmail.com>
2023-12-29 00:56:08 +0300

    reactphysics3D

--- packages/o/old3de/xmake.lua
+++ packages/o/old3de/xmake.lua
@@ -3,17 +3,12 @@ package("old3de")
     add_deps("reactphysics3d")
 
     add_urls("https://github.com/Apsapeh/Old3DEngine.git")
-    --add_versions("1.0", "<shasum256 or gitcommit>")
 
     on_install(function (package)
         local configs = {}
         if package:config("shared") then
             configs.kind = "shared"
         end
+        os.cp("include/*", package:installdir("include"))
         import("package.tools.xmake").install(package, configs)
-    end)
-
-    on_test(function (package)
-        -- TODO check includes and interfaces
-        -- assert(package:has_cfuncs("foo", {includes = "foo.h"})
     end)
