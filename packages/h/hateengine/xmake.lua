31adbaa95419736c8109b40488d96104afc618e0
Timofey Kirichenko <timofeykirichenko@gmail.com>
2024-05-26 14:26:38 +0300

    upd

---
+++ packages/h/hateengine/xmake.lua
@@ -0,0 +1,20 @@
+package("hateengine")
+    set_description("The HateEngine package")
+    add_deps("reactphysics3d 0.9.0")
+    add_deps("glfw 3.3.9")
+    add_deps("glm 1.0.0")
+    add_deps("tinygltf 2.8.13")
+    add_deps("nuklear")
+    add_deps("glu")
+
+    set_urls("https://github.com/Apsapeh/HateEngine.git")
+    add_versions("last", "73a7cfe3e02dc58cb386de2fef956a0b50c3b026")
+
+    on_install(function (package)
+        local configs = {}
+        if package:config("shared") then
+            configs.kind = "shared"
+        end
+        os.cp("include/*", package:installdir("include"))
+        import("package.tools.xmake").install(package, configs)
+    end)
