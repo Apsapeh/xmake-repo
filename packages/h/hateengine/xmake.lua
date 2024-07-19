37f47b2b728b5e7303759a66db2263235072a3ed
Apsapeh <timofeykirichenko@gmail.com>
2024-07-20 02:08:59 +0300

    upd

--- packages/h/hateengine/xmake.lua
+++ packages/h/hateengine/xmake.lua
@@ -1,7 +1,7 @@
 package("hateengine")
     set_description("The HateEngine package")
     add_deps("reactphysics3d 0.9.0")
-    add_deps("glfw 3.4")
+    add_deps("glfw 3.4", {configs = {wayland = is_plat("linux")}})
     add_deps("glm 1.0.0")
     add_deps("tinygltf 2.8.13")
     add_deps("nuklear")
