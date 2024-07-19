f1c4862d2d7d4ccec0491334462cbe280f8fce1b
Apsapeh <timofeykirichenko@gmail.com>
2024-07-20 02:01:13 +0300

    HateEngine GLFW 3.3.9 -> 3.4

--- packages/h/hateengine/xmake.lua
+++ packages/h/hateengine/xmake.lua
@@ -1,11 +1,12 @@
 package("hateengine")
     set_description("The HateEngine package")
     add_deps("reactphysics3d 0.9.0")
-    add_deps("glfw 3.3.9")
+    add_deps("glfw 3.4")
     add_deps("glm 1.0.0")
     add_deps("tinygltf 2.8.13")
     add_deps("nuklear")
     add_deps("glu")
+    add_defines("GLM_ENABLE_EXPERIMENTAL")
 
     set_urls("https://github.com/Apsapeh/HateEngine.git")
     add_versions("last", "73a7cfe3e02dc58cb386de2fef956a0b50c3b026")
