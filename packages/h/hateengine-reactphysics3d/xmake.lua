8276ba843508d925a078e1c3c09fb5dd58827a63
Timofey Kirichenko <timofeykirichenko@gmail.com>
2025-02-17 11:45:18 +0300

    he-rp3d upd

--- packages/h/hateengine-reactphysics3d/xmake.lua
+++ packages/h/hateengine-reactphysics3d/xmake.lua
@@ -3,6 +3,7 @@ package("hateengine-reactphysics3d")
 
     add_urls("https://github.com/Apsapeh/HateEngine-reactphysics3d.git")
     add_versions("0.9.0", "40ee80bc453421faad11be231edbbeb838f93ee2")
+    add_versions("0.10.2", "e692fa2451544e551a4a32972af73d5d52d8a423")
     
     on_install(function (package)
         local configs = {kind="static"}
