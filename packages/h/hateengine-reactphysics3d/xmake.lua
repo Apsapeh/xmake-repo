1a954273b43d8dd35e98aff7326b5418464e7815
Timofey Kirichenko <timofeykirichenko@gmail.com>
2025-02-17 11:43:55 +0300

    Added fork of rp3d - hateengine-rp3d

---
+++ packages/h/hateengine-reactphysics3d/xmake.lua
@@ -0,0 +1,14 @@
+package("hateengine-reactphysics3d")
+    set_description("The reactphysics3d package")
+
+    add_urls("https://github.com/Apsapeh/HateEngine-reactphysics3d.git")
+    add_versions("0.9.0", "40ee80bc453421faad11be231edbbeb838f93ee2")
+    
+    on_install(function (package)
+        local configs = {kind="static"}
+        if package:config("shared") then
+            configs.kind = "shared"
+        end
+        os.cp("include/*", package:installdir("include"))
+        import("package.tools.xmake").install(package, configs)
+    end)
